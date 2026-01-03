39430129a9b6f5d97b6df266e8f241848b0e7d7b
shubham7104 <sksingh71004@gmail.com>
2026-01-03 15:17:30 +0530

    adding q.no_02

--- practice/02-01-2026/solutions.py
+++ practice/02-01-2026/solutions.py
@@ -2,7 +2,6 @@ def exercise_1_squares():
     numbers = [1, 2, 3, 4, 5]
     print(f"Original: {numbers}")
     
-
     squares = []
     for i in numbers:
         squares.append(i**2) 
@@ -10,4 +9,28 @@ def exercise_1_squares():
     print(f"Squared:  {squares}")
 
 
-exercise_1_squares()
+'''TEST'''
+# exercise_1_squares()
+
+
+
+def exercise_2_find_max():
+    numbers = [10, 5, 20, 8, 15]
+    print(f"List: {numbers}")
+    
+    # TODO: Find the largest number without using max()
+    # Hint: Assume the first number is the largest, then loop to check others.
+    largest = numbers[0]
+    for i in range(len(numbers)):
+        if numbers[i] > largest:
+            largest = numbers[i]
+    else:
+        pass
+
+   
+    print(f"Largest Number is: {largest}")
+
+
+
+'''TEST'''
+# exercise_2_find_max()
