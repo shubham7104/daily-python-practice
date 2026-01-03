837a4c6fa18fe5a4c4796197d4b811364bc45d8f
shubham7104 <sksingh71004@gmail.com>
2026-01-03 14:55:42 +0530

    question_no.01

--- practice/02-01-2026/solutions.py
+++ practice/02-01-2026/solutions.py
@@ -1,1 +1,13 @@
-print(f"This is my first repo of this year")
+def exercise_1_squares():
+    numbers = [1, 2, 3, 4, 5]
+    print(f"Original: {numbers}")
+    
+
+    squares = []
+    for i in numbers:
+        squares.append(i**2) 
+        
+    print(f"Squared:  {squares}")
+
+
+exercise_1_squares()
