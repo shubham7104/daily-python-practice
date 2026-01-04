bcc9c7d428d4aaf092eb245d1ce4a3d06b796084
shubham7104 <sksingh71004@gmail.com>
2026-01-04 14:01:51 +0530

    updating qno_02 on day_02

--- practice/03-01-2026/solutions_01.py
+++ practice/03-01-2026/solutions_01.py
@@ -12,5 +12,18 @@ def exercise_easy_vowels():
     print(f"Vowel Count: {len(count)}")
 
 
-
+def exercise_medium_common():
+    list_a = [1, 2, 3, 4, 5]
+    list_b = [4, 5, 6, 7, 8]
+    print(f"List A: {list_a}")
+    print(f"List B: {list_b}")
     
+    # TODO: Create a list containing only numbers found in BOTH lists.
+    # Expected Result: [4, 5]
+    common = []
+    for num in list_a:
+        for char in list_b:
+            if num == char:
+                comman.append(num)
+    
+    print(f"Common Elements: {common}")
