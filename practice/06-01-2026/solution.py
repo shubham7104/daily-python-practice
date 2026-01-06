15909c29a0cf1efeee7d974702885d32916086bd
shubham7104 <sksingh71004@gmail.com>
2026-01-06 11:30:35 +0530

    Adding q_no_01

---
+++ practice/06-01-2026/solution.py
@@ -0,0 +1,15 @@
+def exercise_easy_find_max():
+    numbers = [14, 2, 9, 25, 6, 18]
+    print(f"List: {numbers}")
+    
+    # TODO: Find the maximum number in the list WITHOUT using max().
+    # Hint: Create a variable 'current_max' and set it to the first number.
+    # Loop through the list; if you find a number bigger than 'current_max', update it.
+    current_max = numbers[0]
+    
+    for num in numbers:
+        if num > current_max:
+            num = current_max
+        else:
+            pass
+    print(f"Maximum Value: {current_max}")
