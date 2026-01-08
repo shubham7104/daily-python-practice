41ca3322a4a1e09c810b83b153f585bedad0c670
shubham7104 <sksingh71004@gmail.com>
2026-01-08 17:16:14 +0530

    Adding second question

--- practice/08_01-2026/solutions.py
+++ practice/08_01-2026/solutions.py
@@ -12,3 +12,30 @@ def exercise_easy_count_upper():
     
             
     print(f"Uppercase Count: {count}")
+
+
+
+def exercise_medium_second_largest():
+    numbers = [10, 20, 4, 45, 99]
+    print(f"List: {numbers}")
+    
+    # TODO: Find the SECOND largest number in the list.
+    # Hint: You can do this in two passes:
+    # 1. Find the max number.
+    # 2. Loop again and find the largest number that is NOT the max number.
+    # (Or try to do it in one pass if you are feeling brave!)
+
+    max_value = numbers[0]
+    for num in numbers:
+        if num > max_value:
+            max_value = num
+
+    second_max = 0
+    for num in numbers:
+        if num > second_max and num != max_value:
+            second_max = num
+
+    print(f"Second Largest: {second_max}")
+
+
+exercise_medium_second_largest()
